c622657971ddb93f8c914b31ce2a52393bc190c2
Alban Mulaki <amulaki@pragmaticlinux.org>
2016-07-22 10:16:52 +0200

    Migrate Table comments

---
+++ bertit/database/migrations/2016_07_22_080726_comments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Comments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('ipaddress',19);
+            $table->text('comments');
+            $table->int('likes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
